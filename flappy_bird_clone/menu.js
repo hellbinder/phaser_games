e35ec20a5ca027c7e2885de01874298cfa3e456e
name <email>
2014-04-03 02:01:50 -0400

    Conditional to make spacebar work on two states

--- flappy_bird_clone/menu.js
+++ flappy_bird_clone/menu.js
@@ -5,7 +5,7 @@ var menu_state = {
   create: function () {
     //call start when pressin spacebar
 
-    var space_key = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    space_key = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     space_key.onDown.add(this.start, this);
 
     //variables
@@ -20,10 +20,15 @@ var menu_state = {
     //Check if user played
     if (score > 0) {
       var score_label = game.add.text(x, y + 50, "score " + score, style);
+      score_label.anchor.setTo(0.5, 0.5);
     }
   },
 
   start: function () {
-    game.state.start('play');
+    console.log(game.state);
+    if(game.state.current != 'play') game.state.start('play'); 
+    //checking state to make sure it doesnt restart everytime i press spacebar
+    //have to find a way to be able to bind the same keys for different states.
+    //Could also use another key press. But didn't want to.
   }
 };
